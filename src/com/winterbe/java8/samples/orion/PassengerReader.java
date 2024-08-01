5dc65138cde9c6fc81a33a624a2f3bf7625ab452
atokgoz <atokgoz@rbbn.com>
2024-08-01 10:58:57 +0300

    case 2 done

--- src/com/winterbe/java8/samples/orion/PassengerReader.java
+++ src/com/winterbe/java8/samples/orion/PassengerReader.java
@@ -25,8 +25,15 @@ public class PassengerReader {
             Stream<Passenger> passengerStream = records.stream()
                     .skip(1)
                     .map(PassengerReader::parseToPassenger);
+
             System.out.println("CASE 1 ");
             case1(passengerStream);
+
+            System.out.println("CASE 2 ");
+            case2(records.stream()
+                    .skip(1)
+                    .map(PassengerReader::parseToPassenger));
+
         } catch (IOException | CsvException e) {
             e.printStackTrace();
         }
@@ -44,10 +51,10 @@ public class PassengerReader {
                 record[7],
                 record[8],
                 record[9],
-                record[10],
                 record[11],
                 record[12],
-                record[13]
+                record[13],
+                record[14]
         );
     }
     // case 1  japon yolcularin bulundugu havalanlarini ve burda bulunan yolcularin sayisini yazdir
@@ -60,8 +67,26 @@ public class PassengerReader {
                         });
 
     }
+
     // case 2  yolculugu en cok iptal olan milliyetteki yolculari yazdir
     private static void case2(Stream<Passenger> passengerStream){
+        passengerStream
+                .filter(passenger -> "Cancelled".equals(passenger.getFlightStatus())) // Assuming "Canceled" indicates a canceled flight
+                .collect(Collectors.groupingBy(Passenger::getNationality, Collectors.counting()))
+                .entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+                .limit(1)
+                .forEach(entry -> {
+                    System.out.println("Nationality: " + entry.getKey() + " Count: " + entry.getValue());
+                });
     }
+
+    // case 3
+    private static void case3(Stream<Passenger> passengerStream){
+
+
+    }
+
+
 }
 
